90c29c4da0bd7511b866d22cf13a41a74411a165
Elizabeth Lucky Paradis <lucky.paradis13@gmail.com>
2025-04-17 12:39:30 -0700

    answers

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+// Step 1:
 let code1;
+
+// Step 2:
 code1 = 7 + 3;
-let code2 = (90 - 10) / 2;
-let code3 = 79 % 40;
+
+10;
+// Step 3:
+let code2 = (90 - 10) / 2; // (90 - 10 = 80), 80 / 2 = 40
+
+// Step 4:
+let code3 = 79 % 40; // 79 % 40 = 39
+
+// Step 5:
 let message = "The vault has been secured. The combination is: ";
+
+// Step 6:
 let codeA = code1 + "-" + code2 + "-" + code3;
+
+// Step 7:
 let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
+
+// VM307:1 The vault has been secured.
+// The combination is:
+// VM307:2 10-40-39
+// VM307:3 10-40-39
